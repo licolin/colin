cba05a1a94fbe8bef9022e38a848c1d9ff0280d5
lizhaohuan026901 <lizhaohuan026901@gtjas.com>
2024-06-19 18:57:48 +0800

    666666

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import nextra from 'nextra';
+
+const withNextra = nextra({
+    theme: "nextra-theme-docs",
+    themeConfig: "./theme.config.jsx",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath:process.env.BASE_PATH,
+    assetPrefix:process.env.ASSET_PREFIX,
+    output: "export",
+    images: {
+        unoptimized: true
+    }
+};
+
+export default withNextra(nextConfig);
+// export default nextConfig;
