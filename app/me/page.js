7ae5ff30f9699c659db1c5f57584d1f18fea5295
colin <li_colin@126.com>
2024-10-06 21:06:41 +0800

    fix page.js

--- app/me/page.js
+++ app/me/page.js
@@ -13,13 +13,11 @@ export default function Home() {
                 </div>
                 <p className="mt-3 font-extrabold">Short Bio</p>
                 <div className="ml-5 mt-3 font-serif">
-
                     As a Software Test Development Engineer with 5 years of experience in Python programming, I have built
                     automation frameworks from scratch. I have implemented automated daily regression testing for the company's
                     low-latency securities trading system. With a solid understanding of databases, I am knowledgeable about
                     MySQL and PostgreSQL. I have experience containerizing automation test frameworks and data service modules
                     using Docker. Additionally, I have a good understanding of frontend frameworks such as React and Next.js.
-
                 </div>
 
             </div>
